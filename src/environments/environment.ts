3090bbda3934262a78eea05dbdf0e2be1089dcdf
Phanindra Reddy <phanindras34@gmail.com>
2018-02-14 13:45:48 +0000

    First commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD1vBAg0UlWbGTdzPROIr7Poy__lV3u8aA",
+    authDomain: "party-picker-29feb.firebaseapp.com",
+    databaseURL: "https://party-picker-29feb.firebaseio.com",
+    projectId: "party-picker-29feb",
+    storageBucket: "party-picker-29feb.appspot.com",
+    messagingSenderId: "128576783477"
+  }
+};
