52e910486c54945bcb04822679d61fa996337d81
Phanindra Reddy <phanindras34@gmail.com>
2018-04-21 22:17:15 +0530

    Update environment.prod.ts

--- Resource/src/environments/environment.prod.ts
+++ Resource/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyBajY5-ilBdVmsyDvSSrMhVIE6iDcAFLbU",
-    authDomain: "my-project-e813e.firebaseapp.com",
-    databaseURL: "https://my-project-e813e.firebaseio.com",
-    projectId: "my-project-e813e",
-    storageBucket: "my-project-e813e.appspot.com",
-    messagingSenderId: "410085685776"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: ",
+    messagingSenderId: ""
   }
 };
